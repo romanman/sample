9e82b8950edcd0eb0024dd8431101b9d4515ecb3
romanman <roman.mandeleil@gmail.com>
2014-09-03 11:27:44 +0300

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * www.ethereumJ.com
+ *
+ * @author: Roman Mandeleil
+ * Created on: 03/09/2014 11:24
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hey there");
+    }
+}
